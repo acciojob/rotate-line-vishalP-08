76897ac378ab61d4ad71343c9ba05c2c33f5233d
Vishal.AI <132605163+vishalP-08@users.noreply.github.com>
2023-08-16 17:19:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+const line = document.getElementById('line');
+let rotation = 0;
+
+function rotateLine() {
+  rotation -= 1; // Clockwise rotation
+  line.style.transform = `rotate(${rotation}deg)`;
+  requestAnimationFrame(rotateLine);
+}
+
+rotateLine();
